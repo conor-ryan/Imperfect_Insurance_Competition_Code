dea00a5cf8732b015353c1819d44c8934d3d9b2c
Conor Ryan <cbr52@cornell.edu>
2018-03-15 15:02:01 -0500

    gradient fix for NM

--- Demand_Estimation/MLE_RC.jl
+++ Demand_Estimation/MLE_RC.jl
@@ -534,11 +534,11 @@ function estimate!(d::InsuranceLogit, p0)
         count +=1
         println("Iteration $count at $x")
         #Store Gradient
-        println("Step 1")
+        # println("Step 1")
         δ_cont(x)
-        println("Step 2")
-        ForwardDiff.gradient!(grad, ll, x)
-        println("Gradient: $grad")
+        # println("Step 2")
+        # ForwardDiff.gradient!(grad, ll, x)
+        # println("Gradient: $grad")
         likelihood = ll(x)
         println("likelihood equals $likelihood at $x on iteration $count")
         return likelihood
