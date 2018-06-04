2c5887a8a7c9bf539eb84074fe4edf19795f419a
Conor Ryan <cbr52@cornell.edu>
2018-06-04 12:08:30 -0400

    Little cleanup

--- Demand_Estimation/run_RC_FE.jl
+++ Demand_Estimation/run_RC_FE.jl
@@ -19,14 +19,10 @@ println("Code Loaded")
 include("load.jl")
 # Structre the data
 c = ChoiceData(df,df_mkt;
-    demoRaw=[:AgeFE_31_40,
-            :AgeFE_41_50,
-            :AgeFE_51_64,
-            :Family,
-            :LowIncome],
+    demoRaw=[],
     prodchars=[:Price,:AV],
     prodchars_0=[:Price,:AV],
-    fixedEffects=[:constant,:Firm])
+    fixedEffects=[:Firm])
 
 # Fit into model
 m = InsuranceLogit(c,25)
@@ -45,25 +41,11 @@ par0 = parDict(m,p0)
 grad_2 = Vector{Float64}(length(p0))
 ll =  log_likelihood!(grad_2,m,par0)
 
-
-c2 = ChoiceData(df,df_mkt;
-    demoRaw=[:AgeFE_31_40,
-            :AgeFE_41_50,
-            :AgeFE_51_64,
-            :Family,
-            :LowIncome],
-    prodchars=[:Price,:AV],
-    prodchars_0=[:Price,:AV],
-    fixedEffects=[:Firm])
-
-# Fit into model
-m2 = InsuranceLogit(c2,25)
-
-FE_2 = vcat(0,FEstart[2:length(FEstart)]).+FEstart[1]
-p2 = vcat(γstart,β0start,βstart,σstart,FE_2)
-par2 = parDict(m2,)
-test= Vector{Float64}(length(p2))
-ll_2 =  log_likelihood!(test,m2,p2)
+println("Gradient Test")
+f_ll(x) = log_likelihood(m,x)
+grad_1 = Vector{Float64}(length(p0))
+fval_old = log_likelihood(m,p0)
+ForwardDiff.gradient!(grad_1,f_ll, p0)
 
 
 
@@ -86,16 +68,7 @@ Juno.profiler()
 ## Estimate
 est_res = estimate!(m, p0)
 
-println("Gradient Test")
-f_ll(x) = log_likelihood(m,x)
-grad_1 = Vector{Float64}(length(p0))
-fval = log_likelihood!(grad_2,m,parStart0)
-#
-# @benchmark log_likelihood!(grad_2,m,parStart0)
-# @time log_likelihood!(grad_2,m,parStart0)
 
-fval_old = log_likelihood(m,p0)
-ForwardDiff.gradient!(grad_1,f_ll, p0)
 
 println(fval-fval_old)
 println(maximum(abs.(grad_1-grad_2)))
