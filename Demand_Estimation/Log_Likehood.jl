a6b6e35a8f18360ee8621ef4482f6435fd3371dc
Conor Ryan <cbr52@cornell.edu>
2018-09-01 15:46:45 -0500

    Fixed Likelihood Function

--- Demand_Estimation/Log_Likehood.jl
+++ Demand_Estimation/Log_Likehood.jl
@@ -24,11 +24,17 @@ function log_likelihood{T}(d::InsuranceLogit,p::parDict{T};cont_flag=false)
 
         # Get Market Shares
         s_hat = p.s_hat[idxitr]
-        s_insured = sum(s_hat)
-
         # Fix possible computational error
-        if s_insured>=1
-            s_insured= 1 - 1e-5
+        for k in eachindex(s_hat)
+            if abs(s_hat[k])<=1e-300
+                s_hat[k]=1e-15
+                #println("Hit Share Constraint for person $ind, product $k")
+            end
+        end
+        s_insured = sum(s_hat)
+        if s_insured>=(1-1e-300)
+            s_insured= 1 - 1e-15
+            #println("Hit insured constraint for person $ind")
         end
 
         for i in eachindex(idxitr)
