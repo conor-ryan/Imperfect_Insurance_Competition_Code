772fbae8b35874c38b4f67d0fc8ede1c19a586b0
Conor Ryan <cbr52@cornell.edu>
2019-07-12 11:58:00 -0500

    Run Full Estimation

--- Demand_Estimation/Specification_Run.jl
+++ Demand_Estimation/Specification_Run.jl
@@ -109,35 +109,35 @@ function run_specification_GMM(filename::String,
                             nested = false)
 
     cd("$(homedir())/Documents/Research/Imperfect_Insurance_Competition/Intermediate_Output/Estimation_Parameters/")
-    ## Build Log_Likehood Model
-    # println("Build LL Model")
-    # c_ll = ChoiceData(df,df_mkt,df_risk;
-    #     demoRaw=spec_demoRaw,
-    #     prodchars=spec_prodchars,
-    #     prodchars_0=Vector{Symbol}(undef,0),
-    #     fixedEffects=spec_fixedEffects)
-    #
-    # m_ll = InsuranceLogit(c_ll,1,nested=nested)
-    #
-    # ## Initialize Starting Parameters
-    # γstart = rand(m_ll.parLength[:γ])/10 .-.05
-    # β0start = rand(m_ll.parLength[:β])/10 .-.05
-    # βstart = rand(m_ll.parLength[:γ])/10 .- .05
-    # σstart = rand(m_ll.parLength[:σ])/10 .- .05
-    # FEstart = rand(m_ll.parLength[:FE])/100 .-.005
-    #
-    # p0 = vcat(γstart,β0start,βstart,σstart,FEstart)
-    # println("#### Estimate LL Starting Point ####")
+    # Build Log_Likehood Model
+    println("Build LL Model")
+    c_ll = ChoiceData(df,df_mkt,df_risk;
+        demoRaw=spec_demoRaw,
+        prodchars=spec_prodchars,
+        prodchars_0=Vector{Symbol}(undef,0),
+        fixedEffects=spec_fixedEffects)
 
-    # Estimate
-    # p_ll, fval = newton_raphson_ll(m_ll,p0)
-    #
-    # println("Save LL Result")
+    m_ll = InsuranceLogit(c_ll,1,nested=nested)
+
+    ## Initialize Starting Parameters
+    γstart = rand(m_ll.parLength[:γ])/10 .-.05
+    β0start = rand(m_ll.parLength[:β])/10 .-.05
+    βstart = rand(m_ll.parLength[:γ])/10 .- .05
+    σstart = rand(m_ll.parLength[:σ])/10 .- .05
+    FEstart = rand(m_ll.parLength[:FE])/100 .-.005
+
+    p0 = vcat(γstart,β0start,βstart,σstart,FEstart)
+    println("#### Estimate LL Starting Point ####")
+
+    Estimate
+    p_ll, fval = newton_raphson_ll(m_ll,p0)
+
+    println("Save LL Result")
+    file = "$filename-$rundate-ll.jld2"
+    @save file p_ll
+
     # file = "$filename-$rundate-ll.jld2"
-    # @save file p_ll
-
-    file = "$filename-2019-07-11-ll.jld2"
-    @load file p_ll
+    # @load file p_ll
 
 
 
@@ -169,15 +169,15 @@ function run_specification_GMM(filename::String,
     # @load file p_vec
     # p0 = copy(p_vec)
     W = Matrix(1.0I,m_GMM.parLength[:All]+length(m_GMM.data.tMoments),m_GMM.parLength[:All]+length(m_GMM.data.tMoments))
-    # p_stg1, obj_1 = two_stage_est(m_GMM,p0,W)
+    p_stg1, obj_1 = two_stage_est(m_GMM,p0,W)
 
-    # println("Save First Stage Result")
-    # file = "$filename-$rundate-stg1.jld2"
-    # @save file p_stg1 obj_1
+    println("Save First Stage Result")
+    file = "$filename-$rundate-stg1.jld2"
+    @save file p_stg1 obj_1
     # else
-    println("Load First Stage Result")
-    file = "$filename-2019-07-11-stg1.jld2"
-    @load file p_stg1 obj_1
+    # println("Load First Stage Result")
+    # file = "$filename-$rundate-stg1.jld2"
+    # @load file p_stg1 obj_1
     # end
 
     println("#### Estimate GMM Second Stage ####")
