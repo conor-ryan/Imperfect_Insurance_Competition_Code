c96d1f5e51b2dcb4ffafbec4f749498b0a3ec251
Conor Ryan <cbr52@cornell.edu>
2019-09-12 12:01:01 -0500

    Data back to basic eHealth weights

--- Create_Data_Master.R
+++ Create_Data_Master.R
@@ -13,8 +13,8 @@ source("Code/MEPS_Moments/MEPS_Moments.R")
 
 rm(list=ls())
 source("Code/Demand_Data_Prep/Estimation_Data_Discrete.R",echo=TRUE)
-# source("Code/Risk_Scores/FirmLevelRisk_woSim.R")
-# source("Code/Demand_Data_Prep/Estimation_Data_Discrete.R")
+source("Code/Risk_Scores/FirmLevelRisk_woSim.R")
+source("Code/Demand_Data_Prep/Estimation_Data_Discrete.R")
 
 ###### Data Preperation ######
 rm(list=ls())
