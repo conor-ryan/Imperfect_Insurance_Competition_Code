816b604b125d86817252fa27db53d6e62e34eb68
Conor Ryan <cbr52@cornell.edu>
2019-09-13 13:10:09 -0500

    spec change

--- Solve_Equilibrium/Full_Run.jl
+++ Solve_Equilibrium/Full_Run.jl
@@ -50,12 +50,12 @@ include("$codeDir/Demand_Estimation/utility.jl")
 include("$codeDir/Demand_Estimation/Specification_Run.jl")
 
 filename = "GMM_Estimate_$spec"
-# estimate_demand(filename,rundate,
-#                     halton_draws,
-#                     spec_demoRaw,
-#                     spec_prodchars,
-#                     spec_prodchars_0,
-#                     spec_fixedEffects)
+estimate_demand(filename,rundate,
+                    halton_draws,
+                    spec_demoRaw,
+                    spec_prodchars,
+                    spec_prodchars_0,
+                    spec_fixedEffects)
 
 
 println("##### Estimation Marginal Cost #####")
