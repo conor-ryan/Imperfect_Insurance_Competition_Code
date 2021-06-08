44402d07e4270dab132ac4bf4b6f0ed55a032d44
Conor Ryan <ryan0463@umn.edu>
2021-06-08 13:19:26 -0500

    Date/String allowed in demand specification

--- Demand_Estimation/Specification_Run.jl
+++ Demand_Estimation/Specification_Run.jl
@@ -150,7 +150,7 @@ end
 
 
 function run_specification_GMM(filename::String,
-                            rundate::Date,
+                            rundate::Union(String,Date),
                             df::DataFrame,
                             df_mkt::DataFrame,
                             df_risk::DataFrame;
