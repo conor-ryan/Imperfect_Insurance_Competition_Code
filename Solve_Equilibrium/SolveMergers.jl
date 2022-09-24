f9430c0599bec946d5b435f5bf49063b88e9a412
Conor Ryan <ryan0463@umn.edu>
2022-09-23 21:38:35 -0400

    Distributed functions

--- Solve_Equilibrium/SolveMergers.jl
+++ Solve_Equilibrium/SolveMergers.jl
@@ -260,15 +260,16 @@ function simulate_all_mergers(m::InsuranceLogit,
 
         CSV.write(file,output)
     end
+    rmprocs(workers())
     return nothing
 end
 
-function sendto(p::Int; args...)
+function sendto(p::Int, args...)
     for (nm, val) in args
         @spawnat(p, eval(Main, Expr(:(=), nm, val)))
     end
 end
-function sendto(ps::Vector{Int}; args...)
+function sendto(ps::Vector{Int}, args...)
     for p in ps
         sendto(p; args...)
     end
