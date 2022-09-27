92c1d842dd52bd469ce3b6c0321f292b6d5b5e8b
Conor Ryan <ryan0463@umn.edu>
2022-09-27 19:29:08 -0400

    Parellel working (I think) run without welfare.

--- Solve_Equilibrium/SolveMergers.jl
+++ Solve_Equilibrium/SolveMergers.jl
@@ -235,42 +235,42 @@ function simulate_all_mergers(m::InsuranceLogit,
     # P_Base[:] = f.P_j[:]
 
     #
-    # Solve Baseline Social Planner Problem
-    println("Solve Baseline Planner Problem")
-    solve_SP_parallel!(m,f,sim="SP",voucher=voucher,update_voucher=update_voucher)
-    evaluate_model!(m,f,"All",voucher=voucher)
-
-    # consumer_welfare(m,f,"$(file_stub)_SP_baseline",spec,rundate)
-    trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_baseline",spec,rundate,update_voucher=update_voucher)
-
-    # Output Baseline SP Model
-    file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_baseline.csv"
-    output =  DataFrame(Product=prod_vec,
-                        Price=f.P_j,
-                        Lives=f.S_j)
-    CSV.write(file,output)
-
-
-    ## Solve Baseline Constrained Planner Problem
-    println("Solve Baseline Current Profit Planner Problem")
-    # markets_cp, λ_vec_cp = solve_SP_λ!(m,f,base_profits,markets=[1])
-
-    markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits)
-    evaluate_model!(m,f,"All",voucher=true,update_voucher=false)
-    P_Base_SP_cp[:] = f.P_j[:]
-
-    # println("Model Price: $(f.P_j)")
-    # # P_Base_SP_cp = P_Base[:]
-    # #
-    # consumer_welfare(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate)
-    trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate,update_voucher=update_voucher)
-
-    # Output Baseline Model
-    file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_cp_baseline.csv"
-    output =  DataFrame(Product=prod_vec,
-                        Price=f.P_j,
-                        Lives=f.S_j)
-    CSV.write(file,output)
+    # # Solve Baseline Social Planner Problem
+    # println("Solve Baseline Planner Problem")
+    # solve_SP_parallel!(m,f,sim="SP",voucher=voucher,update_voucher=update_voucher)
+    # evaluate_model!(m,f,"All",voucher=voucher)
+    #
+    # # consumer_welfare(m,f,"$(file_stub)_SP_baseline",spec,rundate)
+    # trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_baseline",spec,rundate,update_voucher=update_voucher)
+    #
+    # # Output Baseline SP Model
+    # file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_baseline.csv"
+    # output =  DataFrame(Product=prod_vec,
+    #                     Price=f.P_j,
+    #                     Lives=f.S_j)
+    # CSV.write(file,output)
+    #
+    #
+    # ## Solve Baseline Constrained Planner Problem
+    # println("Solve Baseline Current Profit Planner Problem")
+    # # markets_cp, λ_vec_cp = solve_SP_λ!(m,f,base_profits,markets=[1])
+    #
+    # markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits)
+    # evaluate_model!(m,f,"All",voucher=true,update_voucher=false)
+    # P_Base_SP_cp[:] = f.P_j[:]
+    #
+    # # println("Model Price: $(f.P_j)")
+    # # # P_Base_SP_cp = P_Base[:]
+    # # #
+    # # consumer_welfare(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate)
+    # trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate,update_voucher=update_voucher)
+    #
+    # # Output Baseline Model
+    # file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_cp_baseline.csv"
+    # output =  DataFrame(Product=prod_vec,
+    #                     Price=f.P_j,
+    #                     Lives=f.S_j)
+    # CSV.write(file,output)
 
     println("Send Data to Workers")
     @eval @everywhere m=$m
