f1ef0bcf95e4cc874d86051a032e58989cdf6540
Conor Ryan <ryan0463@umn.edu>
2022-09-28 20:11:03 -0400

    Problem still somehow existing

--- Solve_Equilibrium/ProcessMergers.R
+++ Solve_Equilibrium/ProcessMergers.R
@@ -16,7 +16,7 @@ load("Intermediate_Output/Simulated_BaseData/simMarketSize.rData")
 prodData = merge(prodData,marketSize,by="Market")
 
 
-
+#### Merger Welfare Data ####
 merger_welfare = NULL
 for (policy in c("Base","RAMan")){
   print(policy)
@@ -105,27 +105,235 @@ merger_welfare[,chg_Tot_Welfare:=chg_CW+chg_Profit]
 merger_welfare[,chg_Tot_Welfaregov:=chg_CW+chg_Profit+chg_Spending]
 
 
-ggplot(merger_welfare[policy=="Base"])+ 
-  geom_point(aes(x=dHHI,y=chg_CW),color="blue",shape=16)+ 
-  geom_point(aes(x=dHHI,y=chg_Tot_Welfare),color="red",shape=17) + 
+# ggplot(merger_welfare[policy=="Base"])+ 
+#   geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+ 
+#   geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) + 
+#   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
+#   facet_wrap(~hhi_category,ncol=1) + 
+#   geom_abline(slope=0,intercept=0) + 
+#   xlab("Predicted Change in HHI")+
+#   ylab("Dollars Per-Person Per-Month")+
+#   theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+#     strip.background = element_blank(),
+#     strip.text = element_text(size=14),
+#     legend.background = element_rect(color=grey(.5)),
+#     legend.title = element_blank(),
+#     legend.text = element_text(size=14),
+#     legend.key.width = unit(.05,units="npc"),
+#     legend.key = element_rect(color="transparent",fill="transparent"),
+#     legend.position = "bottom",
+#     axis.title=element_text(size=14),
+#     axis.text = element_text(size=16))
+
+# ggplot(merger_welfare[policy=="Base"])+ 
+#   geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+ 
+#   geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) + 
+#   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
+#   facet_wrap(~hhi_category,ncol=1) + 
+#   geom_abline(slope=0,intercept=0) + 
+#   xlab("Predicted Change in HHI")+
+#   ylab("Dollars Per-Person Per-Month")+
+#   coord_cartesian(xlim=c(0,1000),ylim=c(-10,10))+
+#   theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+#     strip.background = element_blank(),
+#     strip.text = element_text(size=14),
+#     legend.background = element_rect(color=grey(.5)),
+#     legend.title = element_blank(),
+#     legend.text = element_text(size=14),
+#     legend.key.width = unit(.05,units="npc"),
+#     legend.key = element_rect(color="transparent",fill="transparent"),
+#     legend.position = "bottom",
+#     axis.title=element_text(size=14),
+#     axis.text = element_text(size=16))
+
+ggplot(merger_welfare[policy=="Base"])+
+  geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+
+  geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) +
   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
-  facet_wrap(~hhi_category,ncol=1) + 
-  coord_cartesian(xlim=c(0,1000),ylim=c(-10,10))
+  # facet_wrap(~hhi_category,ncol=1) +
+  geom_abline(slope=0,intercept=0) +
+  xlab("Predicted Change in HHI")+
+  ylab("Dollars Per-Person Per-Month")+
+  theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+    strip.background = element_blank(),
+    strip.text = element_text(size=14),
+    legend.background = element_rect(color=grey(.5)),
+    legend.title = element_blank(),
+    legend.text = element_text(size=14),
+    legend.key.width = unit(.05,units="npc"),
+    legend.key = element_rect(color="transparent",fill="transparent"),
+    legend.position = "bottom",
+    axis.title=element_text(size=14),
+    axis.text = element_text(size=16))
+# 
+# ggplot(merger_welfare[policy=="Base"])+ 
+#   geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+ 
+#   geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) + 
+#   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
+#   # facet_wrap(~hhi_category,ncol=1) + 
+#   geom_abline(slope=0,intercept=0) + 
+#   xlab("Predicted Change in HHI")+
+#   ylab("Dollars Per-Person Per-Month")+
+#   coord_cartesian(xlim=c(0,1000),ylim=c(-10,10))+
+#   theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+#     strip.background = element_blank(),
+#     strip.text = element_text(size=14),
+#     legend.background = element_rect(color=grey(.5)),
+#     legend.title = element_blank(),
+#     legend.text = element_text(size=14),
+#     legend.key.width = unit(.05,units="npc"),
+#     legend.key = element_rect(color="transparent",fill="transparent"),
+#     legend.position = "bottom",
+#     axis.title=element_text(size=14),
+#     axis.text = element_text(size=16))
 
-ggplot(merger_welfare[policy=="RAMan"]) + 
-  geom_point(aes(x=dHHI,y=chg_CW),color="blue",shape=16)+ 
-  geom_point(aes(x=dHHI,y=chg_Tot_Welfare),color="red",shape=17) + 
+# ggplot(merger_welfare[policy=="RAMan"])+ 
+#   geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+ 
+#   geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) + 
+#   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
+#   facet_wrap(~hhi_category,ncol=1) +
+#   geom_abline(slope=0,intercept=0) +
+#   xlab("Predicted Change in HHI")+
+#   ylab("Dollars Per-Person Per-Month")+
+#   theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+#     strip.background = element_blank(),
+#     strip.text = element_text(size=14),
+#     legend.background = element_rect(color=grey(.5)),
+#     legend.title = element_blank(),
+#     legend.text = element_text(size=14),
+#     legend.key.width = unit(.05,units="npc"),
+#     legend.key = element_rect(color="transparent",fill="transparent"),
+#     legend.position = "bottom",
+#     axis.title=element_text(size=14),
+#     axis.text = element_text(size=16))
+
+# ggplot(merger_welfare[policy=="RAMan"])+ 
+#   geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+ 
+#   geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) + 
+#   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
+#   facet_wrap(~hhi_category,ncol=1) +
+#   geom_abline(slope=0,intercept=0) + 
+#   xlab("Predicted Change in HHI")+
+#   ylab("Dollars Per-Person Per-Month")+
+#   coord_cartesian(xlim=c(0,1000),ylim=c(-10,35))+
+#   theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+#     strip.background = element_blank(),
+#     strip.text = element_text(size=14),
+#     legend.background = element_rect(color=grey(.5)),
+#     legend.title = element_blank(),
+#     legend.text = element_text(size=14),
+#     legend.key.width = unit(.05,units="npc"),
+#     legend.key = element_rect(color="transparent",fill="transparent"),
+#     legend.position = "bottom",
+#     axis.title=element_text(size=14),
+#     axis.text = element_text(size=16))
+
+ggplot(merger_welfare[policy=="RAMan"])+ 
+  geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+ 
+  geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) + 
   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
-  facet_wrap(~hhi_category,ncol=1) + 
-  coord_cartesian(xlim=c(0,1000),ylim=c(-15,35))
+  # facet_wrap(~hhi_category,ncol=1) + 
+  geom_abline(slope=0,intercept=0) +
+  xlab("Predicted Change in HHI")+
+  ylab("Dollars Per-Person Per-Month")+
+  theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+    strip.background = element_blank(),
+    strip.text = element_text(size=14),
+    legend.background = element_rect(color=grey(.5)),
+    legend.title = element_blank(),
+    legend.text = element_text(size=14),
+    legend.key.width = unit(.05,units="npc"),
+    legend.key = element_rect(color="transparent",fill="transparent"),
+    legend.position = "bottom",
+    axis.title=element_text(size=14),
+    axis.text = element_text(size=16))
+
+# ggplot(merger_welfare[policy=="RAMan"])+ 
+#   geom_point(aes(x=dHHI,y=chg_CW,color="Consumer Welfare"),shape=16,size=2)+ 
+#   geom_point(aes(x=dHHI,y=chg_Tot_Welfare,color="Total Welfare"),shape=17,size=2) + 
+#   geom_errorbar(aes(x=dHHI,ymin=chg_CW,ymax=chg_Tot_Welfare)) +
+#   # facet_wrap(~hhi_category,ncol=1) + 
+#   geom_abline(slope=0,intercept=0) + 
+#   xlab("Predicted Change in HHI")+
+#   ylab("Dollars Per-Person Per-Month")+
+#   coord_cartesian(xlim=c(0,1000),ylim=c(-10,35))+
+#   theme(#panel.background = element_rect(color=grey(.2),fill=grey(.9)),
+#     strip.background = element_blank(),
+#     strip.text = element_text(size=14),
+#     legend.background = element_rect(color=grey(.5)),
+#     legend.title = element_blank(),
+#     legend.text = element_text(size=14),
+#     legend.key.width = unit(.05,units="npc"),
+#     legend.key = element_rect(color="transparent",fill="transparent"),
+#     legend.position = "bottom",
+#     axis.title=element_text(size=14),
+#     axis.text = element_text(size=16))
 
 
-ggplot(merger_welfare[policy=="Base"]) + 
-  geom_point(aes(x=hhi,y=chg_CW),color="blue")+ 
-  geom_point(aes(x=hhi,y=chg_Tot_Welfare),color="red")
-ggplot(merger_welfare[policy=="RAMan"]) + 
-  geom_point(aes(x=hhi,y=chg_CW),color="blue")+ 
-  geom_point(aes(x=hhi,y=chg_Tot_Welfare),color="red")
+
+#### Merger Price-Effect Data ####
+merger_effects = NULL
+for (policy in c("Base","RAMan")){
+  print(policy)
+  filestub = paste("Estimation_Output/AllMergers_",spec,"-",run,"_",policy,"_",sep="")
+  
+  ### Baseline Market Data ####
+  baseline = fread(paste(filestub,"baseline.csv",sep=""))
+  baseline = merge(baseline,prodData,by="Product")
+  baseline[,insideShare:=Lives/sum(Lives),by="Market"]
+  
+  ## Create HHI baseline data
+  firm_share = baseline[,list(share=sum(insideShare*100)),by=c("Market","ST","Firm")]
+  firm_share[,count:=1]
+  hhi = firm_share[,list(hhi=sum((share)^2),firm_num=sum(count)),by=c("Market","ST")]
+  hhi[,markets:=as.numeric(as.factor(Market))]
+  
+  hhi[,hhi_category:=0]
+  hhi[hhi>=3500,hhi_category:=1]
+  # hhi[hhi>=4000,hhi_category:=2] "2850 < HHI < 4000",
+  hhi[,hhi_category:=factor(hhi_category,levels=c(0,1),
+                            labels=c("HHI < 3500","HHI > 3500"))]
+  
+  #### Iterate Through Mergers ####
+  merger_files = list.files("Estimation_Output",pattern=paste("^AllMergers_",spec,"-",run,"_",policy,sep=""))
+  unique_firms = sort(firm_share[,unique(Firm)])
+  
+  
+  for (i in 1:length(unique_firms)){
+    for (j in 1:(i-1)){
+      if (j==0){next}
+      m1 = unique_firms[j]
+      m2 = unique_firms[i]
+      ## Read in Welfare File
+      merging_party_string = paste(policy,"_",m1,"_",m2,".csv",sep="")
+      file = merger_files[grepl(merging_party_string,merger_files)]
+      if (length(file)==0){next}
+      postmerge = fread(paste("Estimation_Output/",file,sep=""))
+      names(postmerge)[2:length(names(postmerge))] = paste(names(postmerge)[2:length(names(postmerge))],"merge",sep="_")
+      
+      # Merge in HHI and Baseline Data
+      postmerge = merge(baseline,postmerge,by="Product")
+      postmerge = merge(hhi,postmerge,by=c("Market","ST"))
+      
+      #Compute Change variables
+      postmerge[,Price_Effect:=Price_merge-Price]
+      
+      postmerge[,merging_parties:= paste(unique_firms[j],unique_firms[i],sep="-")]
+      postmerge[,policy:=policy]
+      
+      #
+      dHHI = firm_share[Firm%in%c(m1,m2),list(dHHI=2*prod(share),merger=sum(count)),by="Market"]
+      dHHI[merger<2,dHHI:=0]
+      
+      postmerge = merge(postmerge,dHHI[,c("Market","dHHI")],by="Market",all.x=TRUE)
+      postmerge[is.na(dHHI),dHHI:=0]
+      postmerge = postmerge[dHHI>100]
+      merger_effects = rbind(merger_effects,postmerge)
+    }
+  }
+  rm(postmerge,hhi,baseline,firm_share,dHHI,merger_files)
+}
+
 
 
 
--- Solve_Equilibrium/SolveMergers.jl
+++ Solve_Equilibrium/SolveMergers.jl
@@ -662,50 +662,50 @@ function simulate_all_mergers(m::InsuranceLogit,
                         Price=f.P_j,
                         Lives=f.S_j)
     CSV.write(file,output)
-
-    # output = CSV.read(file,DataFrame)
-    # f.P_j = output[!,:Price]
-    # evaluate_model!(m,f,"All",voucher=voucher)
-    # set_voucher!(f,refund=true)
-    # P_Base[:] = f.P_j[:]
-
     #
-    # Solve Baseline Social Planner Problem
-    println("Solve Baseline Planner Problem")
-    solve_SP_parallel!(m,f,sim="SP",voucher=voucher,update_voucher=update_voucher)
-    evaluate_model!(m,f,"All",voucher=voucher)
-
-    # consumer_welfare(m,f,"$(file_stub)_SP_baseline",spec,rundate)
-    trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_baseline",spec,rundate,update_voucher=update_voucher)
-
-    # Output Baseline SP Model
-    file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_baseline.csv"
-    output =  DataFrame(Product=prod_vec,
-                        Price=f.P_j,
-                        Lives=f.S_j)
-    CSV.write(file,output)
+    # # output = CSV.read(file,DataFrame)
+    # # f.P_j = output[!,:Price]
+    # # evaluate_model!(m,f,"All",voucher=voucher)
+    # # set_voucher!(f,refund=true)
+    # # P_Base[:] = f.P_j[:]
     #
-    #
-    ## Solve Baseline Constrained Planner Problem
-    println("Solve Baseline Current Profit Planner Problem")
-    # markets_cp, λ_vec_cp = solve_SP_λ!(m,f,base_profits,markets=[1])
-
-    markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits)
-    evaluate_model!(m,f,"All",voucher=true,update_voucher=false)
-    P_Base_SP_cp[:] = f.P_j[:]
-
-    # println("Model Price: $(f.P_j)")
-    # # P_Base_SP_cp = P_Base[:]
     # #
-    # consumer_welfare(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate)
-    trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate,update_voucher=update_voucher)
-
-    # Output Baseline Model
-    file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_cp_baseline.csv"
-    output =  DataFrame(Product=prod_vec,
-                        Price=f.P_j,
-                        Lives=f.S_j)
-    CSV.write(file,output)
+    # # Solve Baseline Social Planner Problem
+    # println("Solve Baseline Planner Problem")
+    # solve_SP_parallel!(m,f,sim="SP",voucher=voucher,update_voucher=update_voucher)
+    # evaluate_model!(m,f,"All",voucher=voucher)
+    #
+    # # consumer_welfare(m,f,"$(file_stub)_SP_baseline",spec,rundate)
+    # trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_baseline",spec,rundate,update_voucher=update_voucher)
+    #
+    # # Output Baseline SP Model
+    # file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_baseline.csv"
+    # output =  DataFrame(Product=prod_vec,
+    #                     Price=f.P_j,
+    #                     Lives=f.S_j)
+    # CSV.write(file,output)
+    # #
+    # #
+    # ## Solve Baseline Constrained Planner Problem
+    # println("Solve Baseline Current Profit Planner Problem")
+    # # markets_cp, λ_vec_cp = solve_SP_λ!(m,f,base_profits,markets=[1])
+    #
+    # markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits)
+    # evaluate_model!(m,f,"All",voucher=true,update_voucher=false)
+    # P_Base_SP_cp[:] = f.P_j[:]
+    #
+    # # println("Model Price: $(f.P_j)")
+    # # # P_Base_SP_cp = P_Base[:]
+    # # #
+    # # consumer_welfare(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate)
+    # trash = total_welfare_bymkt(m,f,"$(file_stub)_SP_cp_baseline",spec,rundate,update_voucher=update_voucher)
+    #
+    # # Output Baseline Model
+    # file = "$(home_directory)/Research/Imperfect_Insurance_Competition/Estimation_Output/$(file_stub)_SP_cp_baseline.csv"
+    # output =  DataFrame(Product=prod_vec,
+    #                     Price=f.P_j,
+    #                     Lives=f.S_j)
+    # CSV.write(file,output)
 
     println("Send Data to Workers")
     @eval @everywhere m=$m
@@ -774,6 +774,7 @@ function simulate_all_mergers(m::InsuranceLogit,
         solve_model!(m,f,shared_states,sim=sim,voucher=voucher,update_voucher=update_voucher)
         evaluate_model!(m,f,"All",voucher=voucher,update_voucher=update_voucher)
         merger_profits = market_profits(m,f)
+        println(merger_profits[shared_markets])
         P_m[:] = f.P_j[:]
         S_m[:] = f.S_j[:]
 
