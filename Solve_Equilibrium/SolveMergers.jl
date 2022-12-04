593c8859a0de3ed76d48ae73022eebcdb77356e6
Conor Ryan <ryan0463@umn.edu>
2022-12-04 16:20:32 -0500

    Solution debugging

--- Solve_Equilibrium/SolveMergers.jl
+++ Solve_Equilibrium/SolveMergers.jl
@@ -675,6 +675,7 @@ function simulate_all_mergers(m::InsuranceLogit,
     #
     # Solve Baseline Social Planner Problem
     println("Solve Baseline Planner Problem")
+    println("Parameters: voucher: $voucher, update_voucher: $update_voucher,  sim: $sim")
     solve_SP_parallel!(m,f,voucher=voucher,update_voucher=update_voucher)
     evaluate_model!(m,f,"All",voucher=voucher)
 
