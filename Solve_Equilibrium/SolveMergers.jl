d592cd88558bfcb124f9b5f35210854fdd87740e
Conor Ryan <ryan0463@umn.edu>
2022-12-04 20:13:59 -0500

    Run only no policy

--- Solve_Equilibrium/SolveMergers.jl
+++ Solve_Equilibrium/SolveMergers.jl
@@ -54,12 +54,12 @@ function MergersMain(rundate,spec,home_directory)
     par_cost = parMC(mc_est,par_dem,model,costdf)
 
 
-    println("####################################")
-    println("#### Solve Policy Baseline  ####")
-    println("####################################")
-    filestub = "AllMergers_$spec-$(rundate)_Base"
-    simulate_all_mergers(model,df,eq_mkt,par_dem,par_cost,
-                            filestub,policy="Base")
+    # println("####################################")
+    # println("#### Solve Policy Baseline  ####")
+    # println("####################################")
+    # filestub = "AllMergers_$spec-$(rundate)_Base"
+    # simulate_all_mergers(model,df,eq_mkt,par_dem,par_cost,
+    #                         filestub,policy="Base")
     # println("####################################")
     # println("#### Solve Without Risk Adjustment ####")
     # println("####################################")
