7331b6434896cb10b065eaf6f55dc5e558411caa
Conor Ryan <ryan0463@umn.edu>
2022-12-19 14:25:42 -0500

    spec

--- Solve_Equilibrium/Full_Run0.jl
+++ Solve_Equilibrium/Full_Run0.jl
@@ -53,7 +53,7 @@ cost_spec = [:AGE,:AV_std]
 
 # rundate = Dates.today()
 rundate = "2022-12-18"
-spec = "FMC"
+spec = "FM"
 spec_fixedEffects=[:Market_Firm,:Market_Cat]
 println("Running $spec on $rundate")
 
