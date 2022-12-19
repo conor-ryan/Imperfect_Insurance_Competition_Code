7c3efba1e9e09287f2fdadb53acc87482da4fd00
Conor Ryan <ryan0463@umn.edu>
2022-12-18 22:56:33 -0500

    Another atempt:

--- Demand_Estimation/Estimate_Basic.jl
+++ Demand_Estimation/Estimate_Basic.jl
@@ -471,9 +471,9 @@ function newton_raphson_ll(d,p0,W;grad_tol=1e-8,f_tol=1e-8,x_tol=1e-10,
             # if step_size<1e-8
             #     hess_steps=0
             # end
-            if (step_size<x_tol) & (real_hessian==1)
-                x_tol_cnt +=1
-            end
+            # if (step_size<x_tol) & (real_hessian==1)
+            #     x_tol_cnt +=1
+            # end
         end
 
         # if (real_hessian==1) & (hess_steps>0 | close_grad_step==1)
@@ -482,6 +482,11 @@ function newton_raphson_ll(d,p0,W;grad_tol=1e-8,f_tol=1e-8,x_tol=1e-10,
         # else
         #     f_tol_flag = 0
         # end
+        if (step_size<x_tol) & (f_test>=fval)
+            x_tol_cnt +=1
+        else
+            x_tol_cnt = 0
+        end
 
         p_last = copy(p_vec)
         p_vec = copy(p_test)
