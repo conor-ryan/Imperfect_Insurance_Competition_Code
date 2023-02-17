dd2edaee300e68f963d153f212fc295fea5fa07e
ryan0463@umn.edu <ryan0463@umn.edu>
2023-02-17 18:08:54 -0500

    bug

--- Solve_Equilibrium/EvaluateModel.jl
+++ Solve_Equilibrium/EvaluateModel.jl
@@ -605,7 +605,7 @@ function update_derivatives(d::InsuranceLogit,firm::firmData,
 
     #Compute Actual Marginal Costs with Transfers and Adjustment 
     for j in prod_ind, k in prod_ind
-        firm.dCdp_j[j,k] = firm.dCdp_j + firm.dSdp_j[k]*firm.ω_j[k]
+        firm.dCdp_j[j,k] = firm.dCdp_j[j,k] + firm.dSdp_j[k]*firm.ω_j[k]
         firm.dCdp_pl_j[j,k] = firm.dCdp_j[j,k] - firm.dSdp_j[j,k]*TotTransfer[k] - firm.S_j[k]*dTotTransfer[j,k]
     end
 
