c015ca1333701693cdf3c474ba5778e36484e70b
ryan0463@umn.edu <ryan0463@umn.edu>
2023-02-22 15:29:14 -0500

    tuning

--- Solve_Equilibrium/SolveMergers.jl
+++ Solve_Equilibrium/SolveMergers.jl
@@ -664,7 +664,7 @@ function simulate_all_mergers(m::InsuranceLogit,
 
     # Solve Baseline Social Planner Problem
     println("Solve Baseline Planner Problem")
-    solve_SP_parallel!(m,f,voucher=voucher,update_voucher=update_voucher,markets=[4,5,6,7,8,9,10,11,12,13,14])
+    solve_SP_parallel!(m,f,voucher=voucher,update_voucher=update_voucher,markets=[10])
     # solve_SP!(m,f,voucher=voucher,update_voucher=update_voucher)
     evaluate_model!(m,f,"All",voucher=voucher,update_voucher=update_voucher)
 
