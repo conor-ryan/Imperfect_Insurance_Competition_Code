83629131778406ea2ae4bced0b6ae14fc70cf686
ryan0463@umn.edu <ryan0463@umn.edu>
2023-03-06 19:59:58 -0500

    debug

--- Solve_Equilibrium/PlannerProblem.jl
+++ Solve_Equilibrium/PlannerProblem.jl
@@ -298,7 +298,7 @@ function find_λ(m::InsuranceLogit,f::firmData,mkt::Int,
         Π_old = copy(Π_new)
         Π_err = abs(Π_new - Π_target)
         err = Π_err/Π_target
-        println("Got Profit $Π_new at iteration $cnt with λ=$λ_new, target $Π_target, error: $err")
+        println("Got Profit $Π_new at iteration $cnt with λ=$λ_new, target $Π_target, bounds = ($λ_max,$λ_min)")
         if (Π_new<Π_target) & (λ_new==1.0)
             break
         elseif (Π_new>Π_target) & (λ_new==0.0)
