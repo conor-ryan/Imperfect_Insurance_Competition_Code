8b0b7ecca44f49e33741d190b013f02586b29655
ryan0463@umn.edu <ryan0463@umn.edu>
2023-05-05 23:09:21 -0400

    Try another run....

--- Solve_Equilibrium/SolveMergers.jl
+++ Solve_Equilibrium/SolveMergers.jl
@@ -684,9 +684,10 @@ function simulate_all_mergers(m::InsuranceLogit,
     println("Solve Baseline Current Profit Planner Problem")
     # markets_cp, λ_vec_cp = solve_SP_λ!(m,f,base_profits,markets=[1])
     
-    # markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits)
+    markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits)
     # GA Only
-    markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits,markets=[4,5,6,7,8,9,10,11,12,13,14])
+    # markets_cp, λ_vec_cp = solve_SP_λ_parallel!(m,f,base_profits,markets=[4,5,6,7,8,9,10,11,12,13,14])
+    # markets_cp, λ_vec_cp = solve_SP_λ!(m,f,base_profits,markets=[4,5,6,7,8,9,10,11,12,13,14])
     # markets_cp, λ_vec_cp = solve_SP_λ_st!(m,f,base_profits_st,states=["GA"])
     evaluate_model!(m,f,"All",voucher=voucher,update_voucher=update_voucher)
     P_Base_SP_cp[:] = f.P_j[:]
@@ -765,13 +766,13 @@ function simulate_all_mergers(m::InsuranceLogit,
         # merging_parties = ["AETNA","HUMANA"]
 
         ### Only GA MergersMain
-        if !("GA" in shared_states)
-            println("Non-GA Merger")
-            continue
-        else
-            shared_states = ["GA"]
-            shared_markets = [4,5,6,7,8,9,10,11,12,13,14]
-        end
+        # if !("GA" in shared_states)
+        #     println("Non-GA Merger")
+        #     continue
+        # else
+        #     shared_states = ["GA"]
+        #     shared_markets = [4,5,6,7,8,9,10,11,12,13,14]
+        # end
 
  
         println(merging_parties)
@@ -869,4 +870,11 @@ function sendto(ps::Vector{Int}, args...)
         println("Sending to worker $p...")
         sendto(p,args)
     end
+end
+
+
+for i in 4:14
+    println(i)
+    solve_model_mkt!(m,f,i,λ=0.0,sim="SPλ",merg="SP",tol=1e-6,voucher=true,update_voucher=false)
+    println(market_profits(m,f)[i]-base_profits[i])
 end
