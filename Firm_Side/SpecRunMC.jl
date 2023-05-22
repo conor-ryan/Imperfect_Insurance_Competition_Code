02071310db29ac9faba2b0aba3b4143c2f243cc5
ryan0463@umn.edu <ryan0463@umn.edu>
2023-05-22 16:45:11 -0400

    bug

--- Firm_Side/SpecRunMC.jl
+++ Firm_Side/SpecRunMC.jl
@@ -146,7 +146,7 @@ function estimate_marginal_cost(rundate,spec,cost_spec,home_directory)
 
     #### Print Costs Moments
     println("Print Cost Moments...")
-    moments,targets = costMoments(costdf,model,p_stg2,par_est,print_moments=true)
+    moments,targets = costMoments(costdf,m,p_stg2,par_est,print_moments=true)
     file = "$(homedir())/Documents/Research/Imperfect_Insurance_Competition/Estimation_Output/costMoments_$spec-$rundate.csv"
     output =  DataFrame(estimated_moments=moments,
                         targeted_moments = targets)
