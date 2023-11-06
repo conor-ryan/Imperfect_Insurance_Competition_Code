86274a601bdf6a1d85eb06185b57153355a0bbec
ryan0463@umn.edu <ryan0463@umn.edu>
2023-11-06 16:22:13 -0500

    bug

--- Firm_Side/MC_parameters.jl
+++ Firm_Side/MC_parameters.jl
@@ -264,7 +264,7 @@ function parMC(p_MC::Vector{T},par_est::parDict{Float64},d::InsuranceLogit,c::MC
     # Non Risk Costs
     basepars = p_MC[c._baseIndex]' * c.data
     fepars = p_MC[c._feIndex]' * c.fixedEffects
-    metalpars = [0.57,0.60,0.70,0.80,0.90]' * c.metalEffects
+    metalpars = [0.57,0.60,0.70,0.80,0.90] * c.metalEffects
     C_nonrisk = basepars + fepars
     C_nonrisk = exp.(C_nonrisk[:]).*metalpars
 
